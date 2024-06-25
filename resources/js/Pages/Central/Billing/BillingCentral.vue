dc3ea50c752f4b349d359194d9d63e64f92b4eec
jpedrolopesz <jplopeszamonelo@hotmail.com>
2024-06-25 20:00:25 -0300

    commit

--- resources/js/Pages/Central/Billing/BillingCentral.vue
+++ resources/js/Pages/Central/Billing/BillingCentral.vue
@@ -37,21 +37,7 @@ import {
     DropdownMenuTrigger,
 } from "@/Components/ui/dropdown-menu";
 import { Input } from "@/Components/ui/input";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/Components/ui/breadcrumb";
-import { Sheet, SheetContent, SheetTrigger } from "@/Components/ui/sheet";
-import {
-    Pagination,
-    PaginationList,
-    PaginationNext,
-    PaginationPrev,
-} from "@/Components/ui/pagination";
+
 import { Progress } from "@/Components/ui/progress";
 import { Separator } from "@/Components/ui/separator";
 import {
@@ -78,119 +64,6 @@ import AuthenticatedCentralLayout from "../Layouts/AuthenticatedCentralLayout.vu
     <AuthenticatedCentralLayout>
         <div class="flex min-h-screen w-full flex-col">
             <div class="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                <header
-                    class="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6"
-                >
-                    <Sheet>
-                        <SheetTrigger as-child>
-                            <Button
-                                size="icon"
-                                variant="outline"
-                                class="sm:hidden"
-                            >
-                                <PanelLeft class="h-5 w-5" />
-                                <span class="sr-only">Toggle Menu</span>
-                            </Button>
-                        </SheetTrigger>
-                        <SheetContent side="left" class="sm:max-w-xs">
-                            <nav class="grid gap-6 text-lg font-medium">
-                                <a
-                                    href="#"
-                                    class="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                                >
-                                    <Package2
-                                        class="h-5 w-5 transition-all group-hover:scale-110"
-                                    />
-                                    <span class="sr-only">Acme Inc</span>
-                                </a>
-                                <a
-                                    href="#"
-                                    class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                                >
-                                    <Home class="h-5 w-5" />
-                                    Dashboard
-                                </a>
-                                <a
-                                    href="#"
-                                    class="flex items-center gap-4 px-2.5 text-foreground"
-                                >
-                                    <ShoppingCart class="h-5 w-5" />
-                                    Orders
-                                </a>
-                                <a
-                                    href="#"
-                                    class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                                >
-                                    <Package class="h-5 w-5" />
-                                    Products
-                                </a>
-                                <a
-                                    href="#"
-                                    class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                                >
-                                    <Users2 class="h-5 w-5" />
-                                    Customers
-                                </a>
-                                <a
-                                    href="#"
-                                    class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                                >
-                                    <LineChart class="h-5 w-5" />
-                                    Settings
-                                </a>
-                            </nav>
-                        </SheetContent>
-                    </Sheet>
-                    <Breadcrumb class="hidden md:flex">
-                        <BreadcrumbList>
-                            <BreadcrumbItem>
-                                <BreadcrumbLink as-child>
-                                    <a href="#">Dashboard</a>
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator />
-                            <BreadcrumbItem>
-                                <BreadcrumbLink as-child>
-                                    <a href="#">Orders</a>
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator />
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>Recent Orders</BreadcrumbPage>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
-                    <div class="relative ml-auto flex-1 md:grow-0">
-                        <Search
-                            class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground"
-                        />
-                        <Input
-                            type="search"
-                            placeholder="Search..."
-                            class="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-                        />
-                    </div>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger as-child>
-                            <Button
-                                variant="secondary"
-                                size="icon"
-                                class="rounded-full"
-                            >
-                                <CircleUser class="h-5 w-5" />
-                                <span class="sr-only">Toggle user menu</span>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Settings</DropdownMenuItem>
-                            <DropdownMenuItem>Support</DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Logout</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </header>
                 <main
                     class="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"
                 >
@@ -212,7 +85,7 @@ import AuthenticatedCentralLayout from "../Layouts/AuthenticatedCentralLayout.vu
                                     </CardDescription>
                                 </CardHeader>
                                 <CardFooter>
-                                    <Button>Create New Order</Button>
+                                    <Button>Connect Stripe</Button>
                                 </CardFooter>
                             </Card>
                             <Card>
@@ -921,18 +794,6 @@ import AuthenticatedCentralLayout from "../Layouts/AuthenticatedCentralLayout.vu
                                         >November 23, 2023</time
                                     >
                                 </div>
-                                <Pagination class="ml-auto mr-0 w-auto">
-                                    <PaginationList class="gap-1">
-                                        <PaginationPrev
-                                            variant="outline"
-                                            class="h-6 w-6"
-                                        />
-                                        <PaginationNext
-                                            variant="outline"
-                                            class="h-6 w-6"
-                                        />
-                                    </PaginationList>
-                                </Pagination>
                             </CardFooter>
                         </Card>
                     </div>
